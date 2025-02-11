06867ab3b68af4c1aca96abc1ec8b49d0113a5bb
1jay-way <oluwafizzyjay@gmail.com>
2025-02-11 18:42:55 +0100

    Zeal updated
    Super teens update

--- zeal.js
+++ zeal.js
@@ -4,10 +4,12 @@ zeal = {
   "teen": {
     1: {
       topic: "",
-      memory_verse_ref: "",
-      memory_verse_text: "",
-      bible_text_ref: "",
-      bible_text_text: "",
+      memory_verse_ ref: "romans 10vs9",
+      memory_verse_text: "That is thou shall confess with thy mouth the lord jesus and shalt believe in thine heart that God hath raised him from the dead thou shall be save ",
+      bible_text_ref:"Rimans 10 vs 9-10 
+        Act 4 vs 12",
+      bible_text_text: "9for, if you confess* with your mouth that Jesus is Lord and believe in your heart that God raised him from the dead, you will be saved. 10For one believes with the heart and so is justified, and one confesses with the mouth and so is saved. 11For the scripture says, “No one who believes in him will be put to shame.”
+        Acts 4vs12  And there is salvation in no one else, for there is no other name under heaven given among men by which we must be saved",
     },
   },
   "super-teen": {
